feb798adcdb87c0156c5f4144d5a916ee17a002c
underGod247 <underGod247@gmail.com>
2016-01-03 20:06:34 -0500

    added comments to describe functionality of the car class

--- car.py
+++ car.py
@@ -1,2 +1,20 @@
-#car.py is the class for an individual gran prix car
-#Things that this class needs to do:
+'''
+car.py is the class for an individual gran prix car
+Things that this class needs to do:
+Attributes that make up a car:
+- Car number
+- Driver name
+- Weight
+- Age range (club)
+- Checked In (F: ignore time in assigned lane / T: disallow all changes but lane times)
+- Lane Times [R, B, G, Y]
+- (maybe) DQ'd (F: proceed as usual / T: ???)
+- Currently assigned lane (R, B, G, Y, N)
+
+Functions:
+- Calculate average time (intelligently)
+- Manual edit of lane time
+- Undo last run (extend manual edit function)
+- (maybe) edit other attributes / 1 edit function for each attribute
+- Record lane time
+'''
