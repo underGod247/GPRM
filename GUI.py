4dc1399fb49cf45f33b78f34882571c98d2e3192
underGod247 <underGod247@gmail.com>
2016-01-04 21:28:13 -0500

    fixed the serial I/O system (small bugfixes)

--- GUI.py
+++ GUI.py
@@ -17,17 +17,19 @@ finishLine.timeout = 1
 #define subroutines here
 
 #the subroutine to set the port number
-def setPort(portID):
+def setPort():
 	finishLine.port = serialPortString.get()
 	finishLine.open()
+	finishLine.flushInput();
+	finishLine.flushOutput();
 	root.after(500, readSerial)
 	
 #the subroutine to send a command to the finish line (default to the version command)
-def sendCommand(commandChar = 'V'):
+def sendCommand(commandChar = "V"):
 	if(finishLine.isOpen()==False):
 		print("ERROR: Serial Port Not Open")
 		return "break"
-	finishLine.write(commandChar)
+	finishLine.write(commandChar.encode())
 	return "break"
 	
 #the subroutine to just read data off of the serial port
@@ -35,8 +37,8 @@ def readSerial():
 	if(finishLine.isOpen()==False):
 		print("ERROR: Serial Port Not Open")
 		return "break"
-	print(finishLine.read(finishLine.in_waiting))
-	print("\n")
+	if(finishLine.in_waiting > 0):
+		print(finishLine.read(finishLine.in_waiting).decode())
 	root.after(500, readSerial)
 	return "break"
 
@@ -68,15 +70,15 @@ tabs.pack(expand=1, fill="both")
 Label(devTab, text="Port:").grid(row = 0, column = 1, sticky = W)
 Entry(devTab, textvariable = serialPortString).grid(row = 0, column = 2, columnspan = 2, sticky = (E, W))
 Button(devTab, text="Set Port", command = setPort).grid(row = 0, column = 4, sticky = (E, W))
-Button(devTab, text="0 Pg Mem", command=lambda: sendCommand('D')).grid(row = 1, column = 1, sticky = (E, W))
-Button(devTab, text="Version", command=lambda: sendCommand('V')).grid(row = 1, column = 2, sticky = (E, W))
-Button(devTab, text="Results", command=lambda: sendCommand('L')).grid(row = 1, column = 3, sticky = (E, W))
-Button(devTab, text="All Pixels Disp", command=lambda: sendCommand('T')).grid(row = 1, column = 4, sticky = (E, W))
-Button(devTab, text="SensorState", command=lambda: sendCommand('O')).grid(row = 2, column = 1, sticky = (E, W))
-Button(devTab, text="Rotating Disp", command=lambda: sendCommand('Q')).grid(row = 2, column = 2, sticky = (E, W))
-Button(devTab, text="Reset Timer", command=lambda: sendCommand('R')).grid(row = 2, column = 3, sticky = (E, W))
-Button(devTab, text="Flash LED", command=lambda: sendCommand('F')).grid(row = 2, column = 4, sticky = (E, W))
-Button(devTab, text="Start Timer", command=lambda: sendCommand('S')).grid(row = 3, column = 1, sticky = (E, W))
+Button(devTab, text="0 Pg Mem", command=lambda: sendCommand("D")).grid(row = 1, column = 1, sticky = (E, W))
+Button(devTab, text="Version", command=lambda: sendCommand("V")).grid(row = 1, column = 2, sticky = (E, W))
+Button(devTab, text="Results", command=lambda: sendCommand("L")).grid(row = 1, column = 3, sticky = (E, W))
+Button(devTab, text="All Pixels Disp", command=lambda: sendCommand("T")).grid(row = 1, column = 4, sticky = (E, W))
+Button(devTab, text="SensorState", command=lambda: sendCommand("O")).grid(row = 2, column = 1, sticky = (E, W))
+Button(devTab, text="Rotating Disp", command=lambda: sendCommand("Q")).grid(row = 2, column = 2, sticky = (E, W))
+Button(devTab, text="Reset Timer", command=lambda: sendCommand("R")).grid(row = 2, column = 3, sticky = (E, W))
+Button(devTab, text="Flash LED", command=lambda: sendCommand("F")).grid(row = 2, column = 4, sticky = (E, W))
+Button(devTab, text="Start Timer", command=lambda: sendCommand("S")).grid(row = 3, column = 1, sticky = (E, W))
 Button(devTab, text="QUIT", command=getOut).grid(row = 4, column = 1, columnspan =4, sticky = (E, W))
 
 root.mainloop()
